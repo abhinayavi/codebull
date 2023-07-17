2ea561849fcc5d5e854b9f0d9695fb2baf946c02
abhinayavi <abhinayavi1947@gmail.com>
2023-07-17 22:56:20 +0530

    Commit 2

---
+++ src/main/java/example/TestJava.java
@@ -0,0 +1,21 @@
+package example;
+
+public class TestJava {
+
+    public static void main(String args[]) {
+
+        test();
+
+    }
+
+    public static void test() {
+
+        String name = "Abhinay";
+
+        if (name.equalsIgnoreCase("avi")){
+            System.out.println("name= Abhinay");
+
+        }
+    }
+
+}
